336a572e38ef0f35ae1ed7f642f67a4c3035f65a
Dragoton <89391420+Dragoton@users.noreply.github.com>
2021-12-16 09:25:09 +0100

    footer_desktop_without_some

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const card = document.querySelector(".card");
+const cardtoggle = document.querySelector(".arrow1");
+console.log("this is loaded");
 
+cardtoggle.onclick = function () {
+  card.classList.toggle("active");
+};
